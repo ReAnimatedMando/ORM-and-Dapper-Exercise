3501aa62736c08c3621a02c0d5181a503872b37f
Aaron Tanguma <the_guma@yahoo.com>
2026-01-06 10:34:58 -0700

    created constructor for IDbConnection

---
+++ ORM-Dapper/DapperDepartmentRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Data;
+using Dapper;
+
+namespace ORM_Dapper;
+
+public class DapperDepartmentRepository : IDepartmentRepository
+{
+    private readonly IDbConnection _conn;
+
+    public DapperDepartmentRepository(IDbConnection conn)
+    {
+        _conn = conn;
+    }
+
+    public IEnumerable<Department> GetAllDepartments()
+    {
+        return _conn.Query<Department>("SELECT * FROM departments");
+    }
+}
