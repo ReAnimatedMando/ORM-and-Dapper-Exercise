6ff61dce99a0d4ff614afaee74ca73f357d2b35f
Aaron Tanguma <the_guma@yahoo.com>
2026-01-06 10:02:34 -0700

    add config code in main method

--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace ORM_Dapper
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ORM_Dapper
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connString = config.GetConnectionString("DefaultConnection");
         }
     }
 }
