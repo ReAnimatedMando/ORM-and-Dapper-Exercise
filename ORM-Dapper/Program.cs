c117043bed4d344038341e74907cf4fd6c81f17c
Aaron Tanguma <the_guma@yahoo.com>
2026-01-06 13:00:36 -0700

    add calls for products

--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -16,20 +16,38 @@ namespace ORM_Dapper
             string connString = config.GetConnectionString("DefaultConnection");
 
             IDbConnection conn = new MySqlConnection(connString);
+            #region Department Section
+            // var departmentRepo = new DapperDepartmentRepository(conn);
+            //
+            // departmentRepo.InsertDepartment("Aarons New Department");
+            //
+            // var departments = departmentRepo.GetAllDepartments();
+            //
+            // foreach (var department in departments)
+            // {
+            //     Console.WriteLine(department.DepartmentID);
+            //     Console.WriteLine(department.Name);
+            //     Console.WriteLine();
+            //     Console.WriteLine();
+            // }
+            #endregion
 
-            var departmentRepo = new DapperDepartmentRepository(conn);
+            var productRepository = new DapperProductRepository(conn);
 
-            departmentRepo.InsertDepartment("Aarons New Department");
+            var products = productRepository.GetAllProducts();
 
-            var departments = departmentRepo.GetAllDepartments();
-
-            foreach (var department in departments)
+            foreach (var product in products)
             {
-                Console.WriteLine(department.DepartmentID);
-                Console.WriteLine(department.Name);
+                Console.WriteLine(product.ProductID);
+                Console.WriteLine(product.Name);
+                Console.WriteLine(product.Price);
+                Console.WriteLine(product.CategoryID);
+                Console.WriteLine(product.OnSale);
+                Console.WriteLine(product.StockLevel);
                 Console.WriteLine();
                 Console.WriteLine();
             }
+            
         }
     }
 }
